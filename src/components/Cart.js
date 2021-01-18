e956f783db0233dc487a548f47f3d73a6a0bd170
Herbert Ssevume <you@example.com>
2021-01-18 21:15:56 +0000

    Third commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Cart = ({product,price,qty,image,deleteditem,id,add_qty}) => {
+const Cart = ({product,price,qty,image,deleteditem,id,add_qty,remove_qty}) => {
     
      const amount = price*qty
   return (
@@ -14,7 +14,7 @@ const Cart = ({product,price,qty,image,deleteditem,id,add_qty}) => {
                     <span><b>Product</b> {qty}x{product}</span><br/>
                     <span><b>Price:</b> £ {amount.toFixed(2)}</span> 
                     <a className="btn-floating btn-small add-btn" onClick={()=>add_qty({id,qty:Number(qty)+1})}><i className="material-icons">add</i></a>
-                    <a className="btn-floating btn-small   "><i className="material-icons">remove</i></a>
+                    <a className="btn-floating btn-small " onClick={()=>remove_qty({id,qty:Number(qty)-1})}><i className="material-icons">remove</i></a>
                   <button type="submit" className="delete delete-btn right " onClick ={()=>deleteditem(id)}><i className="material-icons ">delete_forever</i></button>
             </div> 
           
--- src/components/Products.js
+++ src/components/Products.js
@@ -30,14 +30,20 @@ const deleteditem = (e)=>{
 //Increment the qty of an item by one for each click
  const add_qty = (e)=>{
     cart.find(v=>v._id ===e.id).qty=e.qty
-    setCart([...cart])
-   
+    setCart([...cart]) 
 }
+
+//Decrement the qty of an item by one for each click
+const remove_qty = (e)=>{
+  cart.find(v=>v._id ===e.id).qty=e.qty
+  setCart([...cart]) 
+}
+
 //Calculate total price of items in the basket
 const totalprice = cart.reduce((a, c) => a + c.price * c.qty, 0)
 
 //Basket.Map through selected products
-const basket = cart.map(item=>(<div key={item._id}><Cart qty={item.qty} product={item.product} price={item.price} image={item.image} deleteditem={deleteditem} id={item._id} add_qty={add_qty}/></div>))
+const basket = cart.map(item=>(<div key={item._id}><Cart qty={item.qty} product={item.product} price={item.price} image={item.image} deleteditem={deleteditem} id={item._id} add_qty={add_qty} remove_qty={remove_qty}/></div>))
 
 //List products. Item component is used in the map() method
 const display = productdata.map(item=>(<div key={item._id}><Item image={item.image} name={item.product} price={item.price} id={item._id} selectedid={selectedid}/></div>))
@@ -50,7 +56,7 @@ const display = productdata.map(item=>(<div key={item._id}><Item image={item.ima
         </div>  
 
         <div className=' col s12 m2'>
-        <i class="material-icons ">shopping_cart</i><b>Basket cost: </b><span>£{totalprice.toFixed(2)}</span>
+        <i class="material-icons ">shopping_cart</i><b>Basket cost: </b><span className='totalprice'>£{totalprice.toFixed(2)}</span>
              {basket}
         </div>
          
